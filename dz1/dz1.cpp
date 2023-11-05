50d3dda7889b27b6ca18aa7f7db647902442e9cf
LimboDancer <ekko2363@gmail.com>
2023-11-06 00:46:16 +0500

    fix1

--- dz1/dz1.cpp
+++ dz1/dz1.cpp
@@ -7,6 +7,9 @@
 * *Создать структуру(struct) объединяющую: union (int, float, char) и 3 - и битовых поля(флага)
 указывающими какого типа значение в данный момент содержится в объединении(isInt, isFloat, isChar).Продемонстрировать пример использования в коде этой структуры.
 */
+
+
+
 short int sfoo = 1;
 int ifoo = 11;
 long long bigNum = 12312459112;
@@ -43,9 +46,28 @@ int main()
 			float fnum;
 			char string;
 		};
-		bool isInt;
-		bool isFloat;
-		bool isChar;
+		unsigned int isInt:1;
+		unsigned int isFloat:1;
+		unsigned int isChar:1;
+		typeOn type;
 	};
+
+	gameProcces gProc;
+	gProc.type.fnum = 77.7;
+	gProc.type.string = 'w';
+	gProc.type.number = 999;
+
+	std::cout << gProc.type.fnum << std::endl;
+	std::cout << gProc.type.number << std::endl;
+	std::cout << gProc.type.string << std::endl;
+
+	gProc.isChar = gProc.type.string == 'w' ? 1 : 0;
+	gProc.isFloat = gProc.type.fnum == 77.7 ? 1 : 0;
+	gProc.isInt = gProc.type.number == 999 ? 1 : 0;
+
+	std::cout << "CHAR!" << gProc.isChar << std::endl;
+	std::cout << "FLOAT!" << gProc.isFloat << std::endl;
+	std::cout << "INT!" << gProc.isInt << std::endl;
+
 	
 }
